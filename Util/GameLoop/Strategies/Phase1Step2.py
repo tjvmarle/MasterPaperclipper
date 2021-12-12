3ede25ff98ce0f7237f11a45185ce94f0fde3eb6
tjvmarle <metalananas@gmail.com>
2021-12-12 15:01:24 +0100

    Added seperate handling for high prio projects

--- Util/GameLoop/Strategies/Phase1Step2.py
+++ Util/GameLoop/Strategies/Phase1Step2.py
@@ -5,6 +5,7 @@
 from Util.Resources.CashSpender import CashSpender
 from Util.Resources.PriceWatcher import PriceWatcher
 from Util.Resources.ThreadClicker import ThreadClicker
+from Util.Resources.TourneyOrganiser import TourneyOrganiser
 from Util.Resources.TrustSpender import TrustSpender
 from Webpage.PageState.PageActions import PageActions
 from Webpage.PageState.PageInfo import PageInfo
@@ -13,14 +14,12 @@ from Util.Files.Config import Config
 import time
 
 
-Alive = True
-
-
 class Phase1Step2():
     def __init__(self, pageInfo: PageInfo, pageActions: PageActions) -> None:
         self.info = pageInfo
         self.actions = pageActions
 
+        self.highPrioProjects = Config.get("highPriorityProjects")
         self.projects = Config.get("phaseTwoProjects")
 
         self.start = Config.get("Gamestart")
@@ -28,36 +27,60 @@ class Phase1Step2():
         self.fundsHandler = CashSpender(self.info, self.actions)
         self.pricer = PriceWatcher(self.info, self.actions)
         self.trustee = TrustSpender(self.info, self.actions)
-        # TODO: yomi tournament organizer
-        self.runners = (self.thread, self.fundsHandler, self.pricer, self.trustee)
-
-    def __kill(self):
-        """Temporary switch to end the entire session."""
-        TS.print("End goal reached!")
-        self.thread.kill()
-        global Alive
-        Alive = False
+        self.yomi = TourneyOrganiser(self.info, self.actions)
+        self.runners = (self.thread, self.fundsHandler, self.pricer, self.trustee, self.yomi)
+        self.projectNotifiers = [self.fundsHandler.getCallback()]  # Ugly, but works for now
 
     def __buyProjects(self):
         if not self.projects:
             return
+
+        boughtProject = []
+        for project in self.highPrioProjects:
+            if self.actions.isEnabled(project):
+                time.sleep(0.4)  # The buttons 'blink' in
+                TS.print(f"High prio - Buying {project}.")
+                self.actions.pressButton(project)
+                boughtProject.append(project)
+
+        for project in boughtProject:
+            self.highPrioProjects.remove(project)
+            if project in self.projects:
+                self.projects.remove(project)
 
         projectBttn = self.projects[0]
         if self.actions.isEnabled(projectBttn):
             time.sleep(0.4)  # The buttons 'blink' in
             TS.print(f"Buying {projectBttn}")
             self.actions.pressButton(projectBttn)
-
-            if projectBttn == "Hypno Harmonics":
-                # Current kill point
-                TS.print(f"Reached {projectBttn} in {TS.deltaStr(self.start)}")
-                self.__kill()
-
             self.projects.pop(0)
+            boughtProject.append(projectBttn)
+
+            if projectBttn in self.highPrioProjects:  # This should rarely occur
+                TS.print(f"Race condition encountered, removeing {projectBttn}.")
+                self.highPrioProjects.remove(projectBttn)
+
+        for project in boughtProject:
+            self.notify(project)
+
+    def addNotifier(self, callback) -> None:
+        self.projectNotifiers.append(callback)
+
+    def notify(self, project: str):
+        for callback in self.projectNotifiers:
+            callback(project)
 
     def tick(self):
         self.__buyProjects()
 
         for runner in self.runners:
             runner.tick()
-        return Alive
+
+        if self.info.getInt("Trust") >= 25:
+            # Current kill point
+            TS.print(f"Reached 25+ trust in {TS.deltaStr(self.start)}")
+            TS.print("End goal reached!")
+            self.thread.kill()
+            return False
+
+        return True
