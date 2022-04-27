2838d604c4e195f32eb0def6fc4e45e7a28419ec
tjvmarle <metalananas@gmail.com>
2022-04-27 10:29:32 +0200

    OrderedEnum class to better track internal states.

---
+++ Util/Resources/OrderedEnum.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+
+class OrderedEnum(Enum):
+    """Base class to create ordered Enums. This allows comparing larger/smaller, which can be used to order internal
+    states in a chronological expected order."""
+
+    def __ge__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value >= other.value
+        return NotImplemented
+
+    def __gt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value > other.value
+        return NotImplemented
+
+    def __le__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value <= other.value
+        return NotImplemented
+
+    def __lt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value < other.value
+        return NotImplemented
